af8f14be69e5be90a1d0708adc6ec62584427a06
J.Y. Yang <xyang@mail.pec.com.tw>
2020-08-13 15:04:07 +0800

    using Dashboard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import Dashboard from './Dashboard';
+import theme from './theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
