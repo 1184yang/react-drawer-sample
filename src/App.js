db340f004f682657f213f45a5c245dadaa99e412
J.Y. Yang <xyang@mail.pec.com.tw>
2020-08-14 10:49:09 +0800

    change port to 6464

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import { Container, Typography, Box, Link } from '@material-ui/core';
-import ProTip from './ProTip';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://1184yang.github.io/">
-        J.Y. Yang
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      <Dashboard />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
       <CssBaseline />
-      <Dashboard />
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
